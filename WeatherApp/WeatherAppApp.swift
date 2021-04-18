c38faf51509448ad2fb9b0936f1f0d32bc6ab17f
Sizwe Maluleke <sizwes.maluleke@gmail.com>
2021-04-18 10:10:19 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Sizwe on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
